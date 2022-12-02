866d7a019c1173a760c7e0550760bb425ffe5a9b
Brian David Peralta Arriaga <74439172+Brian-David-01@users.noreply.github.com>
2022-12-02 15:41:28 -0600

    Nueva actualizacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes {''} Con GitHub {''}
+            Seguimiento Pacientes {''}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
