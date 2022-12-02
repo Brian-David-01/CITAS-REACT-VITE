e914d8607625871660315ceb69cf99257e21d592
Brian David Peralta Arriaga <74439172+Brian-David-01@users.noreply.github.com>
2022-12-02 14:08:54 -0600

    Primer commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+const Error = ({ children }) => {
+  return (
+    <div className='bg-red-800 text-white text-center p-3 uppercase mb-3 rounded-md font-bold'>
+        {children}
+    </div>
+  )
+}
+
+export default Error
