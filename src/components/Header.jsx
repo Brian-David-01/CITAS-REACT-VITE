12cdf30f429505d697b4285133ba95c69e32aab0
Brian David Peralta Arriaga <74439172+Brian-David-01@users.noreply.github.com>
2022-12-29 21:48:58 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
             Seguimiento Pacientes {''}
-            <span className="text-indigo-600">Veterinaria</span>
+            <span className="text-indigo-600">Veterinaria con ReactJS y Vite</span>
         </h1>
     )
 }
