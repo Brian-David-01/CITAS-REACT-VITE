c7f8f2e64c39c6ac10b0db3be2fbe6d55324a98c
Brian David Peralta Arriaga <74439172+Brian-David-01@users.noreply.github.com>
2022-12-30 22:41:17 -0600

    Update ListadoPacientes.jsx

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -27,7 +27,7 @@ const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
           <h2 className='font-black text-3xl text-center'>No hay pacientes</h2>
           <p className='text-lg mt-5 mb-10 text-center'>
               Comienza agregando pacientes {''}
-            <span className='text-indigo-600 font-bold'>y aparecerán en este lugar</span>
+            <span className='text-indigo-600 font-bold'>y aparecerán en esta sección</span>
           </p>
         </>
       )}
