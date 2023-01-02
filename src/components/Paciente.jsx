5fe63b5458570d134428e7a3086d0d9fe1135d02
Brian David Peralta Arriaga <74439172+Brian-David-01@users.noreply.github.com>
2023-01-01 19:59:15 -0600

    Update Paciente.jsx

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -36,11 +36,11 @@ const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
           <button 
             type="button" 
             className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
-            onClick={ () => {setPaciente(paciente)}}>Editar</button>
+            onClick={ () => {setPaciente(paciente)}}>Editar Paciente</button>
           <button 
             type="button" 
             className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-            onClick={handleEliminar}>Eliminar</button>
+            onClick={handleEliminar}>Eliminar Paciente</button>
         </div>
       </div>
   )
