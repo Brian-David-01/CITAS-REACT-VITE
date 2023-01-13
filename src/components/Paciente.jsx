4c6ccb5cee3b3647f7c3f621b7cfa23790ebaddd
Brian David Peralta Arriaga <74439172+Brian-David-01@users.noreply.github.com>
2023-01-12 22:47:35 -0600

    Update Paciente.jsx

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -12,7 +12,7 @@ const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
 
   return (
     <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: {''}
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre de mascota: {''}
           <span className='font-normal normal-case'>{nombre}</span>
         </p>
 
